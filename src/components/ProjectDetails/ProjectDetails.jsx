e12a9d928bbcef96f3e34c1f8157becad7618396
genesisscv <genesisscv@gmail.com>
2022-09-18 17:00:04 +1000

    fixing project page

--- src/components/ProjectDetails/ProjectDetails.jsx
+++ src/components/ProjectDetails/ProjectDetails.jsx
@@ -4,21 +4,10 @@ import "./ProjectDetails.css";
 export default function ProjectDetails(props) {
   const { projectData } = props;
   return (
-        <div className="details-container">
+    <div className="details-container">
       <img src={projectData.image} alt="project" />
       <h2>{projectData.title}</h2>
       <h3>Created at: {projectData.date_created}</h3>
-      <h3>{`Status: ${projectData.is_open}`}</h3>
-      <h3>Pledges:</h3>
-      <ul>
-        {projectData.pledges.map((pledgeData, key) => {
-          return (
-            <li>
-              {pledgeData.amount} from {pledgeData.supporter}
-            </li>
-          );
-        })}
-      </ul>
     </div>
   );
 }
--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -2,22 +2,31 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import ProjectDetails from "../components/ProjectDetails/ProjectDetails";
 
-
 function ProjectPage() {
-    const [projectData, setProjectData] = useState({pledges: [] });
-    const { id } = useParams();
+  const [projectData, setProjectData] = useState({ pledges: [] });
+  const { id } = useParams();
 
-    useEffect(() => {
-        fetch(`${process.env.REACT_APP_API_URL}projects/${id}`)
-        .then((results) => {
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}projects/${id}`)
+      .then((results) => {
         return results.json();
-        })
-        .then((data) => {
+      })
+      .then((data) => {
         setProjectData(data);
-        });
-    }, []);
-    return (
-        <ProjectDetails projectData={projectData} />
-    );
+      });
+  }, []);
+  
+  
+  return (
+  <ProjectDetails projectData={projectData} />
+  <div>
+  <h3>Pledges:</h3>
+      
+        {projectData.pledges.map((pledgeData, key) => {
+          return (
+            
+              {pledgeData.amount} from {pledgeData.supporter}
+        
+            </div>)
 }
 export default ProjectPage;
